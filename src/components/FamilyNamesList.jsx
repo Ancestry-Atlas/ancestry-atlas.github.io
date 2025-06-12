d3f1f2b37aa35e9aa7f560152a95babefabb4675
Miguel Rivas <github.5yeil@simplelogin.fr>
2025-06-11 22:07:49 -0700

    adding pretty

--- src/components/FamilyNamesList.jsx
+++ src/components/FamilyNamesList.jsx
@@ -1,30 +1,42 @@
-import "nano-grid/dist/nanogrid.js";
-import gColors from "nano-grid/dist/gcolors.js";
-import { Link } from "react-router-dom";
+import 'nano-grid/dist/nanogrid.js'
+import gColors from 'nano-grid/dist/gcolors.js'
+import { Link } from 'react-router-dom'
 
 export default function FamilyNamesList({ familyNames, onDelete }) {
   return (
     <nn-pilar className="listing family">
-      <nn-caja padding="1rem" max-width="600px">
+      <nn-caja
+        padding="1rem"
+        max-width="600px"
+      >
         <h2>Family Names</h2>
         <nn-desplazador>
           <ul className="repeater list">
-            {familyNames.map((name) => (
+            {familyNames.map(name => (
               <li key={`${name.id}`}>
                 <nn-fila>
-                  <nn-pilar size="35px" className="index">
+                  <nn-pilar
+                    size="35px"
+                    className="index"
+                  >
                     {name.id}
                   </nn-pilar>
-                  <nn-pilar size="100% - 35px * 3" className="preview">
+                  <nn-pilar
+                    size="100% - 35px * 3"
+                    className="preview"
+                  >
                     {name.label}
                   </nn-pilar>
                   <nn-pilar size="35px">
                     <Link to={`/family-names/edit/${name.id}`}>
-                      <nn-btn color={gColors["shamrock-green"].hex}>E</nn-btn>
+                      <nn-btn color={gColors['shamrock-green'].hex}>E</nn-btn>
                     </Link>
                   </nn-pilar>
                   <nn-pilar size="35px">
-                    <nn-btn color="#ff5555" onClick={() => onDelete(name.id)}>
+                    <nn-btn
+                      color="#ff5555"
+                      onClick={() => onDelete(name.id)}
+                    >
                       X
                     </nn-btn>
                   </nn-pilar>
@@ -34,9 +46,9 @@ export default function FamilyNamesList({ familyNames, onDelete }) {
           </ul>
         </nn-desplazador>
         <Link to="/family-names/edit">
-          <nn-btn color={gColors["sunglow"].hex}>Add Family Name</nn-btn>
+          <nn-btn color={gColors['sunglow'].hex}>Add Family Name</nn-btn>
         </Link>
       </nn-caja>
     </nn-pilar>
-  );
+  )
 }
